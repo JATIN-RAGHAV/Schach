49f598b5425d60744e05971c93e9acc8f2177243
Walrus <jrag137@gmail.com>
2026-03-12 12:16:59 +0530

    fix: onMessage handler was closured

--- src/frontend/src/lib/interfaces/customHooks.ts
+++ src/frontend/src/lib/interfaces/customHooks.ts
@@ -23,7 +23,7 @@ export interface gameStartState{
         connect:(color:colors,gameType:gameTypes,increment:number,setStart:React.Dispatch<React.SetStateAction<boolean>>) => void
 }
 
-export const useGame = create<gameStartState>((set,get) => ({
+export const useGame = create<gameStartState>((set) => ({
     gameType:null,
     color:null,
     gameIncrement:null,
@@ -37,78 +37,80 @@ export const useGame = create<gameStartState>((set,get) => ({
         setSocket:(socket:WebSocket) => set({socket}),
         setBoard:(board:Board) => set({board}),
         connect:(color:colors,gameType:gameTypes,increment:number,setStart:React.Dispatch<React.SetStateAction<boolean>>) => {
-        console.log("from connect")
-        const gameState = get().gameState;
-        const board = get().board;
-        const res = startGame(color,gameType,increment);
-        const setBoard = get().setBoard;
-        const setGameState = get().setGameState;
-        res.onopen = () => {
-            set({color,gameType,gameIncrement:increment,socket:res})
-            setStart(true);
-        }
-        res.onerror = (err) => {
-            console.error("Error starting game",err);
-            alert("Error starting game");
-        }
-        /*
-         * A message could mean a couple of things
-         * 1-> Message to indicate start of game
-         * 2-> Message to indicate end of game
-         * 3-> Message to give a move by other player
-         * 4-> Message to confirm the move made by current player
-         */
-        res.onmessage = ((message:MessageEvent<any>) => {
-            let data = JSON.parse(message.data);
-            const {inMove,pieceMoved,setInMove,setWinner} = useOnMessageHandlerState.getState();
-            // Handle starting of game
-            if(gameState == gameStateType.waiting){
-                data = data as startGameResponse;
-                if(data.start){
-                    set({gameState:gameStateType.running})
-                    set({color:data.color});
-                }
+            const res = startGame(color,gameType,increment);
+            res.onopen = () => {
+                set({color,gameType,gameIncrement:increment,socket:res})
+                setStart(true);
             }
-            else{
-                data = data as moveSocketResponse;
-                // Handle move made by current player being conformed
-                if(inMove){
-                    setInMove(false)
-                    if(!data.error){
+            res.onerror = (err) => {
+                console.error("Error starting game",err);
+                alert("Error starting game");
+            }
+            /*
+             * A message could mean a couple of things
+             * 1-> Message to indicate start of game
+             * 2-> Message to indicate end of game
+             * 3-> Message to give a move by other player
+             * 4-> Message to confirm the move made by current player
+             */
+            res.onmessage = ((message:MessageEvent<any>) => {
+                console.log("On message handler working")
+                let data = JSON.parse(message.data);
+                const {inMove,pieceMoved,setInMove,setWinner,} = useOnMessageHandlerState.getState();
+                const {gameState,setGameState,board,setBoard,setColor,color} = useGame.getState();
+                // Handle starting of game
+                if(gameState == gameStateType.waiting){
+                    console.log(gameState)
+                    console.log("waiting for start of game")
+                    data = data as startGameResponse;
+                    if(data.start){
+                        console.log("game starting")
+                        setGameState(gameStateType.running);
+                        setColor(data.color);
+                    }
+                }
+                else{
+                    console.log("not start of game.")
+                    data = data as moveSocketResponse;
+                    // Handle move made by current player being conformed
+                    if(inMove){
+                        console.log("In a move")
+                        setInMove(false)
+                        if(!data.error){
+                            const moveIndex = moveCharsToIndex(data.move);
+                            if(moveIndex){
+                                const [sRow,sCol,tRow,tCol] = moveIndex;
+                                let newBoard = structuredClone(board);
+                                newBoard[tRow][tCol] = pieceMoved;
+                                newBoard[sRow][sCol] = Pieces.NN;
+                                setBoard(newBoard);
+                            }
+                        }
+                    }
+                    // Handle other player making a move
+                    else if(!data.over){
                         const moveIndex = moveCharsToIndex(data.move);
                         if(moveIndex){
                             const [sRow,sCol,tRow,tCol] = moveIndex;
                             let newBoard = structuredClone(board);
-                            newBoard[tRow][tCol] = pieceMoved;
                             newBoard[sRow][sCol] = Pieces.NN;
-                            setBoard(newBoard);
+                            newBoard[tRow][tCol] = board[sRow][sCol];
+                            setBoard(newBoard)
+                        }
+                    }
+                    // Handle game ending
+                    else{
+                        setGameState(gameStateType.ended);
+                        const colorC = color ? color : colors.White;
+                        if(data.winner){
+                            setWinner(colorC)
+                        }
+                        else{
+                            setWinner(colorC);
                         }
                     }
                 }
-                // Handle other player making a move
-                else if(!data.over){
-                    const moveIndex = moveCharsToIndex(data.move);
-                    if(moveIndex){
-                        const [sRow,sCol,tRow,tCol] = moveIndex;
-                        let newBoard = structuredClone(board);
-                        newBoard[sRow][sCol] = Pieces.NN;
-                        newBoard[tRow][tCol] = board[sRow][sCol];
-                        setBoard(newBoard)
-                    }
-                }
-                // Handle game ending
-                else{
-                    setGameState(gameStateType.ended);
-                    const colorC = color;
-                    if(data.winner){
-                        setWinner(colorC)
-                    }
-                    else{
-                        setWinner(colorC == colors.White ? colors.Black : colors.White);
-                    }
-                }
-            }
-        })
-    }
+            })
+        }
 }
                                                            ))
